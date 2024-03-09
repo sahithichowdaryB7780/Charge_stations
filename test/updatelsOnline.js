af3c5ada96dae79961183c37ee18be0fe4e1fe77
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-03-09 13:49:37 +0530

    Update updatelsOnline.js

--- test/updatelsOnline.js
+++ test/updatelsOnline.js
@@ -1,10 +1,8 @@
 const {expect} = require('chai');
-const request = require('supertest');
 const {connect, dropDB, closeConnectionDB} = require('../connection.js');
 const {app, seturi} = require('../server.js');
 const {EVConnectors} = require('../database/data.js');
-
-
+const request = require('supertest');
 describe('Update isOnline Field in Connectors', () => {
   before(async () => {
     delete process.env.uri;
