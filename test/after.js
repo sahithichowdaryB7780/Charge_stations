97166fe24310b1d8cf19139ef697358f4177e34d
sahithichowdaryB7780 <106342836+sahithichowdaryB@users.noreply.github.com>
2024-03-17 09:20:39 +0530

    updated p

--- test/after.js
+++ test/after.js
@@ -1,9 +1,9 @@
 const {closeConnectionDB} = require('../connection.js');
 
-function cleanup() {
-  // this.timeout(30000); // Increase timeout to 30 seconds
-  closeConnectionDB();
-  // .then(() => done());
+function cleanup(done) {
+  this.timeout(30000); // Increase timeout to 30 seconds
+  closeConnectionDB()
+      .then(() => done());
 }
 
 module.exports = cleanup;
